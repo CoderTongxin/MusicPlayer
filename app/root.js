8bcfbdbe2147ee6a1f8426d179634833153c3927
XieTongxin <gin199413@gmail.com>
2018-01-01 00:07:40 +0800

    create components2

--- app/root.js
+++ app/root.js
@@ -5,11 +5,41 @@ import Progress from './components/progress'
 
 
 class Root extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            progress: '-'
+        };
+    }
+
+     componentDidMount(){
+
+         $('#player').jPlayer({
+             ready:function(){
+                 $(this).jPlayer('setMedia',{
+                     mp3:'http://res.webftp.bbs.hnol.net/zhangyu/music/cd114/01.mp3'
+                 }).jPlayer('play');
+             },
+             supplied:'mp3',
+             wmode:'window'
+         });
+         $('#player').bind($.jPlayer.event.timeupdate, (e) =>{
+              this.setState({
+                  progress : Math.round(e.jPlayer.status.currentTime)
+              });
+         })
+     },
+
+    componentWillUnmount(){
+        $('#player').unbind($.jPlayer.event.timeupdate);
+    }
+
     render(){
         return(
             <div>
                 <Header />
-                <Progress progress={1}> </Progress>
+                <div id="player"> </div>
+                <Progress progress={this.state.progress}> </Progress>
             </div>
 
         )
