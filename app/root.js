ce851141deff3bb5d50eeeaa7672c884f6b9273f
XieTongxin <gin199413@gmail.com>
2017-12-31 23:00:59 +0800

    create components

---
+++ app/root.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/header';
+import Progress from './components/progress'
+
+
+
+class Root extends React.Component{
+    render(){
+        return(
+            <div>
+                <Header />
+                <Progress progress={1}> </Progress>
+            </div>
+
+        )
+    }
+}
+
+
+export default Root;
